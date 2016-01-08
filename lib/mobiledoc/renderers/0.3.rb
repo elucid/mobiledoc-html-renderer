7a53383f1594c892c35923329ed429d7ef73bb93
Justin Giancola <justin.giancola@gmail.com>
2016-01-08 03:59:24 +0000

    Begin work on 0.3.0 renderer

---
+++ lib/mobiledoc/renderers/0.3.rb
@@ -0,0 +1,7 @@
+require "mobiledoc/renderers/0.2"
+
+module Mobiledoc
+  class Renderer_0_3 < Renderer_0_2
+    MOBILEDOC_VERSION = '0.3.0'
+  end
+end
--- lib/mobiledoc_html_renderer.rb
+++ lib/mobiledoc_html_renderer.rb
@@ -1,5 +1,6 @@
 require "mobiledoc_html_renderer/version"
 require "mobiledoc/renderers/0.2"
+require "mobiledoc/renderers/0.3"
 require "mobiledoc/cards/unknown"
 require "mobiledoc/atoms/unknown"
 
@@ -66,6 +67,8 @@ module Mobiledoc
       case version
       when '0.2.0', nil
         Renderer_0_2.new(mobiledoc, state).render
+      when '0.3.0', nil
+        Renderer_0_3.new(mobiledoc, state).render
       else
         raise StandardError.new(%Q[Unexpected Mobiledoc version "#{version}"])
       end
